31e151a44e13a513e3fe4e7a423cf02b7c86bd51
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-11-20 16:19:23 +0200

    file

--- src/leapYearCalculator/nancy-leap_year_checker.js
+++ src/leapYearCalculator/nancy-leap_year_checker.js
@@ -1,20 +1,20 @@
 function leapYearChecker(number) {
     // Write your code here
         if (year /  4) {
-            console.log("true")
+            return true
         }
         else if (year /100 ) {
-            console.log("true");
+            return true;
         }
     
         else if (year / 400) {
-            console.log("true");
+            return true;
         }
         else if (year / 366) {
-            console.log("true");
+            return true;
         }
         else if (year / 365) {
-            console.log("true");
+            return true;
         }
     }
 
@@ -23,4 +23,4 @@ function leapYearChecker(number) {
 
 var year = [1700, 1800, 1900, 2100, 2200, 2300, 2500, 2600];
     leapYearChecker
-console.log(year);
+console.log();
