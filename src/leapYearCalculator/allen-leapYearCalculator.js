a796468fac5a3abfb07eab155af5412559a50f8f
allen-teta <148436556+allen-teta@users.noreply.github.com>
2023-11-20 16:02:49 +0200

    my work

---
+++ src/leapYearCalculator/allen-leapYearCalculator.js
@@ -0,0 +1,20 @@
+function leapYearCalculator(year) {
+if (year%4===0 && year%100===0 && year%400===0) {
+    
+    return true;
+}else{
+
+    return false
+}
+    
+};
+console.log(leapYearCalculator(1700));
+
+
+module.exports = {
+    leapYearCalculator
+}
+
+
+
+
