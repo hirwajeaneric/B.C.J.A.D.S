ae874f39c82b161858e8f93b61833e217c14de2a
M0111Ange <angemukundente@gmail.com>
2023-11-20 16:19:16 +0200

    leap_year updated

--- src/ange-leap_year_calculator.js
+++ src/ange-leap_year_calculator.js
@@ -22,28 +22,6 @@ module.exports = {
 
 
 
-// function {
-//     if (number % 4 === 0 && number % 100 === 0 && number % 400 === 0)// Write your code here
-    
-// };
-
-// function leapYearChecker(number){
-   
-// }
-// // Write the code to call your function here
-
-
-// module.exports = {
-//     leapYearCalculator
-// }
-
-// function leapYearChecker(number){
-//     return (number % 4 ===0 && number% 100 !== 0 || number %400===0)
-// }  
-// console.log(leapYearChecker(2000));
-// console.log(leapYearChecker(2000));
-// console.log(leapYearChecker(2000));
-// console.log(leapYearChecker(2000));
 
 
 
