2763d25196fa7c807fc42298ce50b847f0e3faee
Karangwa <hopemutesi250@gmail.com>
2023-11-21 15:23:58 +0200

    modified javaScript.js

--- src/leapYearCalculator/javaScript.js
+++ src/leapYearCalculator/javaScript.js
@@ -1,11 +1,29 @@
-function leapYear(year){
-    if(year%4==0){
-        console.log("this is leap year");
+// function isLeapYear(year) {
+//     // Check if the year is divisible by 4
+//     if (year % 4 !== 0) {
+//         return false;
+//     }
+    
+//     // If the year is divisible by 100, it must also be divisible by 400 to be a leap year
+//     if (year % 100 === 0 && year % 400 !== 0) {
+//         return false;
+//     }
+    
+//     // If the year is divisible by 4 and not divisible by 100 (or divisible by 400), it's a leap year
+//     return true;
+// }
+ 
+
+function isLeapYear(year){
+
+if(year%4==0){
+    console.log(true);
+}
+    else if(year%100 ==0 && year%400==0){
+        console.log(true);
     }
     else{
-        console.log("oops not a leap year")
+        console.log(false);
     }
-
 }
-
-leapYear(2005);
+isLeapYear(2005);
