2d5de8a313dede8b29ee22f487095a80a439d89b
lenlanaa <tracuna005@gmail.com>
2023-11-21 15:24:14 +0200

    changes of the file
    changed the name

--- src/leapYearCalculator/Laena-leapyear_calculator.js
+++ src/leapYearCalculator/Laena-leapyear_calculator.js
@@ -1,6 +1,10 @@
 
-function leapYearChecker(number){
+function leapYearCalculator(number){
     return (number % 4==0 && number % 100!==0)|| number % 400===0;
 }
-console.log(leapYearChecker(2600));
-console.log(leapYearChecker(1600));
+console.log(leapYearCalculator(2600));
+console.log(leapYearCalculator(1600));
+
+module.exports = {
+    leapYearCalculator
+}
