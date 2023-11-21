1fc3d0c9928673377bf42ebcf54d607a26530cc2
alinecoding <uwerairakozealine@gmail.com>
2023-11-21 16:44:06 +0200

    my work

---
+++ src/tuesday/aline_21_nov.js
@@ -0,0 +1,36 @@
+//q1.
+
+function countVowels(inputString){
+    var vowels = ['a','i','u','o','e'];
+    var lowercaseString = inputString.toLowerCase();
+  
+    var vowelCount = 0;
+    for (var i = 0; i < lowercaseString.length; i++) {
+        if (vowels.includes(lowercaseString[i])) {
+          vowelCount++;
+        }
+      }
+    
+      return vowelCount;
+    }
+    console.log(countVowels("aline uwera"));
+
+
+    //q2.
+
+    function sortArray(arr){
+        return arr.sort(function(a,b){
+            return a-b;
+         });
+        }
+        console.log(sortArray([0,1,2,3,4,5]));
+
+
+
+    //q3.
+    function convertsTemp(c,f){
+        tFahrenheit = (tCelcius * 9/5) + 32;
+        return tFahrenheit;
+    }
+    console.log(convertsTemp);
+    
