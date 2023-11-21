f47bcb289ac7238a7e11bab1f28da07ab2bbc9e4
dosite-dev <dositecyiza@gmail.com>
2023-11-21 15:22:49 +0200

    dosite-leap_year_calculator.js

--- src/leapYearCalculator/dosite-leap_year_calculator.js
+++ src/leapYearCalculator/dosite-leap_year_calculator.js
@@ -1,6 +1,5 @@
 function leapYearChecker(year) {
     // Write your code here
-    var year;
     
     if(year % 4 === 0 && year % 100 === 0 && year % 400 === 0 ){
         // console.log("True") ;
