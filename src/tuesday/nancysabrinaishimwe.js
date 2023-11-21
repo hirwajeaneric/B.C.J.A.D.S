a69d021b9c4b8dffb719f9f570e0877eb2ea721b
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-11-21 16:20:46 +0200

    first commit

---
+++ src/tuesday/nancysabrinaishimwe.js
@@ -0,0 +1,26 @@
+// Write codes for the first function bellow this line
+
+ function countWords(str, position, char) {
+    if (position < 0 || position >= str.length) {
+      return str;
+    }
+    return str.substring(0, position) + char + str.substring(position + 1);
+   }
+   
+   console.log(countWords("Hello", 1, 'a')); 
+
+// Write codes for the second function bellow this line
+function sortingArray(arr){
+   if(arr>=0,; arr--) 
+}
+
+
+// Write codes for the third function bellow this line
+
+
+
+module.exports = {
+    vowelCounter,
+    arraySorter,
+    temperatureConverter
+}
