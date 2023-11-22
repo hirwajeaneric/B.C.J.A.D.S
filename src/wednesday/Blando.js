773c748229d55c825548886595fc6b71c1f5464b
UBlandine <ugirablando@gmail.com>
2023-11-22 16:10:28 +0200

    Wednesday

---
+++ src/wednesday/Blando.js
@@ -0,0 +1,15 @@
+//Write a JavaScript program that takes two numbers and an operator (+, -, *, /) as input and uses a switch case to perform the corresponding operation.
+//The function name should be: "calculator"
+function twoNumbers(a,b){
+    switch(true){
+        case add = a + b:
+        console.log(value);
+        break;
+        case value = a - b:
+            console.log(value);
+            break;
+            case div = 
+            case
+        case 
+    }
+}
