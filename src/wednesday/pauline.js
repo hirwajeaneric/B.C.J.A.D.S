39bb4d5f8516c787784fac4b6eb5866a60f08e72
pauline1998 <jeannepauline81@gmail.com>
2023-11-22 16:19:20 +0200

    wednesday quizz

---
+++ src/wednesday/pauline.js
@@ -0,0 +1,21 @@
+function calculator(arr) {
+    switch (operator, num1 ,num2){
+        case "+":
+            console.log(num1 +num2);
+
+        break;
+        case "-":
+            console.log(num1-num2);
+        break; 
+        case "*":
+            console.log(num1 * num2);
+        break
+        case "/":
+            console.log(num1 / num2);
+        break;
+        default:
+        console.log("no answer");
+    } 
+}
+
+console.log(calculator(6, 4));
