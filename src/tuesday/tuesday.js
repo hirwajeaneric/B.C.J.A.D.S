b83d96e1bae61f26e278b3ae8d9dd7d8f356e433
allen-teta <148436556+allen-teta@users.noreply.github.com>
2023-11-22 16:11:53 +0200

    calculator

--- src/tuesday/tuesday.js
+++ src/tuesday/tuesday.js
@@ -3,18 +3,18 @@
 
 // Write codes for the first function bellow this line
 function countVowels(inputString){
-    var vowel= ["a","i","o","u","e"];
-    var vow = inputString . toLowerCase();
-    var count = 0;
-    for( let i=0; i < vow.length; i++){
-      if(vowel.includes(vow[i])){
-          count++;
-      }
+  var vowel= ["a","i","o","u","e"];
+  var vow = inputString . toLowerCase();
+  var count = 0;
+  for( let i=0; i < vow.length; i++){
+    if(vowel.includes(vow[i])){
+        count++;
     }
-    return count;
-    
   }
-  console.log(countVowels("TETA"));
+  return count;
+  
+}
+console.log(countVowels("TETA"));
 
 
 // Write codes for the second function bellow this line
---
+++ src/wednesday/allen.js
@@ -0,0 +1,29 @@
+// Put your answer bellow this line
+function calculator(a,b){
+    var operator ="*+-/";
+    switch (calculator) {
+        case 1: "+"
+        console.log(a+b);
+            
+            break;
+            case 2: "*"
+            console.log(a*b);
+            break;
+            case 3: "-"
+            console.log(a-b);
+            break;
+            case 4: "/"
+            console.log(a/b);
+    
+        default:
+            console.log("invalid input")
+            break;
+    }
+}
+console.log(calculator(2,5));
+
+// Put your code above this line.
+// Do not delete the lines of code bellow
+module.exports = {
+    calculator
+}
