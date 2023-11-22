82930934ada95b46ff0b53ce49a0c8c55ad4f40c
celine123-code <mumararungucelyne@gmail.com>
2023-11-22 06:21:11 -0800

    add

--- src/wednesday/answer_template.js
+++ src/wednesday/answer_template.js
@@ -1,5 +1,30 @@
 // Put your answer bellow this line
+function calculator(num1,num2,operator) {
 
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+            break;
+         case '-':
+            return num1 - num2;
+            break;
+         case '/':
+            if(num2 !== 0){
+                return num1 / num2;
+            }
+            else{
+                return " "
+            }
+             break;
+        case '*':
+             return num1 * num2;
+            break;
+        default:
+            return "Invalid input"
+    }
+}
+
+console.log ("Answer is: ",calculator(6,7,'*'));
 
 // Put your code above this line.
 // Do not delete the lines of code bellow
