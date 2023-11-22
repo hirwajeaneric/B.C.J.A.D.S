f462db44c91a7398da421a55d3cbd2cc05750d36
ingabire-byte <ingabirejoy25@gmail.com>
2023-11-22 06:13:05 -0800

    work

---
+++ src/wednesday/Joy.js
@@ -0,0 +1,32 @@
+function operations(num1,operation,num2){
+    switch (operation){
+        case '+':
+            return num1 + num2;
+
+            break;
+
+            case '-':
+            return num1 - num2;
+            break;
+
+            case 'x':
+                return num1*num2;
+                break;
+
+                case '/':
+                    if(num1===0){
+                        return 'operation is impossible';
+                    }else if( num2===0){
+                            return 'opreration is invalid '
+                        }else{
+                            return num1/num2;
+                        }
+
+                        }
+                    }
+                    const num1 = 6;
+                    const num2 = 2;
+                    const operation='+';
+                    console.log(operations(6,2));
+
+            
